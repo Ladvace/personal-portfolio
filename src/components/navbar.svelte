db5dfa301b8d79e570f82c6ed2e7ee0f729e0b4f
Gianmarco <cavallogianmarco@gmail.com>
2021-03-20 14:33:11 +0100

    responsive navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,13 +1,27 @@
 <script>
   import { link } from "svelte-navigator";
+  import Burger from "./hamburger.svelte";
   import Logo from "../../public/logo.svg";
+
+  let opened = false;
+  $: console.log(opened);
 </script>
 
-<nav class="NavBar">
-  <a href="/" use:link>
-    <Logo class="logo" width="30" />
-  </a>
-  <div class="buttons">
+<nav class={opened ? "NavBar open" : "NavBar"}>
+  <div class="innerContainer">
+    <a href="/" use:link>
+      <Logo class="logo" width="30" />
+    </a>
+    <div class="burger">
+      <Burger bind:open={opened} />
+    </div>
+    <div class="buttons">
+      <a class="button" href="/">Home</a>
+      <a class="button" href="/projects">Projects</a>
+      <a class="button" href="/about">About</a>
+    </div>
+  </div>
+  <div class="responsiveButtons buttons">
     <a class="button" href="/">Home</a>
     <a class="button" href="/projects">Projects</a>
     <a class="button" href="/about">About</a>
@@ -18,20 +32,59 @@
   :global(.logo) {
     cursor: pointer;
   }
-  .NavBar {
+
+  .open {
+    flex-direction: column !important;
+    align-items: center !important;
+    height: 250px !important;
+    transition: height 0.2s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  }
+
+  .innerContainer {
     display: flex;
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    height: 90px;
+    max-width: 900px;
+    box-sizing: border-box;
+  }
+  .innerContainer :global(a) {
+    height: 30px;
+  }
+
+  .NavBar {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
     max-width: 900px;
     box-sizing: border-box;
     padding: 20px;
+    height: 90px;
+    overflow: hidden;
+    transition: height 0.2s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  }
+
+  .responsiveButtons {
+    display: none !important;
+    margin-top: 20px;
   }
   .buttons {
-    display: flex;
+    display: none;
     justify-content: space-between;
     align-items: center;
+  }
+
+  .responsiveButtons {
+    width: 100%;
+    display: flex !important;
+    flex-direction: column;
+  }
+
+  .responsiveButtons .button {
+    width: 100%;
+    text-align: center;
   }
 
   .buttons .button {
@@ -48,12 +101,28 @@
     cursor: pointer;
   }
 
+  .burger :global(button) {
+    margin: 0;
+  }
   @media (min-width: 900px) {
     .NavBar {
       padding: 20px 0 0 0;
       display: flex;
-      justify-content: space-between;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
       max-width: 900px;
+    }
+
+    .buttons {
+      display: flex;
+    }
+
+    .NavBar .burger {
+      display: none !important;
+    }
+    .responsiveButtons {
+      display: none !important;
     }
   }
 </style>
